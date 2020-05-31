9ae1a7cdbe3c2b65bb6793f27bebe6c6c5dd3006
하형 <51150005+maryoh2003@users.noreply.github.com>
2020-05-31 10:00:40 +0900

    [ADD] files

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    클론받을때마다 오배건
+    </>
+  );
+}
+
+export default App;
---
+++ src/styles/typography/typography_scheme.js
@@ -0,0 +1,46 @@
+// typography
+
+export const typography = {
+  font: {
+    Noto_Sans_Thin: 'Noto Sans Thin',
+    Noto_Sans_Light: 'Noto Sans Light',
+    Noto_Sans_Regular: 'Noto Sans Regular',
+    Noto_Sans_Medium: 'Noto Sans Medium',
+    Noto_Sans_SemiBold: 'Noto Sans SemiBold',
+    Noto_Sans_Bold: 'Noto Sans Bold',
+    Noto_Sans_Black: 'Noto Sans Black',
+    Noto_Sans_KR_Thin: 'Noto Sans KR Thin',
+    Noto_Sans_KR_Light: 'Noto Sans KR Light',
+    Noto_Sans_KR_DemiLight: 'Noto Sans KR DemiLight',
+    Noto_Sans_KR_Regular: 'Noto Sans KR Regular',
+    Noto_Sans_KR_Medium: 'Noto Sans KR Medium',
+    Noto_Sans_KR_Bold: 'Noto Sans KR Bold',
+    Noto_Sans_KR_Black: 'Noto Sans KR Black'
+  },
+  size: {
+    s1: '12px', // detail contents
+    s2: '14px', // content1
+    s3: '16px', // content2
+    m1: '18px', // title1
+    m2: '20px', // title2
+    m3: '22px', // title3
+    m4: '24px', // title4
+    m5: '28px',
+    l1: '32px',
+    l2: '40px',
+    l3: '48px',
+    code: '90px',
+  },
+};
+
+export const sizeArray = [
+  Number(typography.size.s1),
+  Number(typography.size.s2),
+  Number(typography.size.s3),
+  Number(typography.size.m1),
+  Number(typography.size.m2),
+  Number(typography.size.m3),
+  Number(typography.size.l1),
+  Number(typography.size.l2),
+  Number(typography.size.l3)
+];
